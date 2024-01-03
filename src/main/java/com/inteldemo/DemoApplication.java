7584c6e8d55c7409fc22b6b0cfe20597aa3e6cfa
psnem <piyush.sharma.1992@gmail.com>
2024-01-04 00:19:25 +0530

    "Commit with intelij"

---
+++ src/main/java/com/inteldemo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.inteldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
